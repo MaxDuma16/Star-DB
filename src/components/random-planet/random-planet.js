49ec7e9ab2926a315fa945d1fb75a8d458157a3c
Max <maxduma16@gmail.com>
2021-02-02 15:25:39 +0200

    add logic random planet and spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
+import Spinner from '../spinner/spinner';
 
 export default class RandomPlanet extends Component {
-
   swapiService = new SwapiService();
 
   state = {
-    planet: {}
+    planet: {},
+    loading: true
   };
 
   constructor() {
@@ -16,22 +17,36 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = (planet) => {
-    this.setState({ planet });
+    this.setState({ planet, loading: false });
   };
 
   updatePlanet() {
-    const id = 12;
+    const id = Math.floor(Math.random()*25)+1;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded);
   }
 
   render() {
-
-    const { planet: {id, name, population, rotationPeriod, diameter} } = this.state;
+    const { planet, loading } = this.state;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !loading ? <PlanetView planet={planet}/> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {spinner}
+        {content}
+      </div>
+    );
+  }
+}
+
+
+const PlanetView = ({planet}) => {
+  const { id, name, population, rotationPeriod, diameter } = planet;
+ 
+  return (
+    <React.Fragment>
         <img className="planet-image"
              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
         <div>
@@ -51,8 +66,6 @@ export default class RandomPlanet extends Component {
             </li>
           </ul>
         </div>
-      </div>
-
-    );
-  }
-}
+    </React.Fragment>
+  );
+};
